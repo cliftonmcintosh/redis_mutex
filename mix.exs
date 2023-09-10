8fea5a9cebd5eefc312e25c8765ab3b6765e0e31
Clifton McIntosh <clifton.mcintosh@podium.com>
2023-09-10 11:23:07 -0500

    Add hammox for testing

--- mix.exs
+++ mix.exs
@@ -32,7 +32,8 @@ defmodule RedisMutex.Mixfile do
 
       # Dev and test dependencies
       {:credo, "~> 1.6", only: [:dev, :test]},
-      {:ex_doc, "~> 0.29", only: :dev}
+      {:ex_doc, "~> 0.29", only: :dev},
+      {:hammox, "~> 0.7.0", only: :test}
     ]
   end
 
